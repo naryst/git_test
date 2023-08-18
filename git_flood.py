f8a82e824a0b09cc3afa4b2981167ef080acf123
naryst <nikitasergeev692@gmail.com>
2023-08-18 17:35:27 +0300

    more flood

--- git_flood.py
+++ git_flood.py
@@ -2,4 +2,5 @@ n = 100
 
 for i in range(n):
     with open(str(i)+'.flood', 'w') as flood_file:
-        flood_file.write('hello number' + str(i))
+        for j in range(1000):
+            flood_file.write('hello number ' + str(i) + ' line number ' + str(j) + '\n')
